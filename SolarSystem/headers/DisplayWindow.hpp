1dbe8e2638a2ad796bd54e003a586159794e3faf
ArturasDruteika <arturas.druteika@gmail.com>
2023-04-17 22:00:15 +0300

    Added new window DisplayWindow which deals only with 3D rendering

---
+++ SolarSystem/headers/DisplayWindow.hpp
@@ -0,0 +1,21 @@
+#ifndef SOLARSYSTEMSETUP_DISPLAYWINDOW_HPP
+#define SOLARSYSTEMSETUP_DISPLAYWINDOW_HPP
+
+
+#include "imgui.h"
+
+
+class DisplayWindow
+{
+public:
+    DisplayWindow();
+    ~DisplayWindow();
+
+    void RunMainWindow();
+
+private:
+
+};
+
+
+#endif //SOLARSYSTEMSETUP_DISPLAYWINDOW_HPP

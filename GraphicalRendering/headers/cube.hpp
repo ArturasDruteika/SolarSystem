b87581e3e972e368388db210f11605ac3b469239
ArturasDruteika <arturas.druteika@gmail.com>
2023-05-31 23:12:57 +0300

    Added a new project for graphical rendering of objects using vtk

---
+++ GraphicalRendering/headers/cube.hpp
@@ -0,0 +1,26 @@
+#ifndef SOLARSYSTEMSETUP_CUBE_HPP
+#define SOLARSYSTEMSETUP_CUBE_HPP
+
+#if defined (_WIN32)
+#if defined(GRAPHICALRENDERING_EXPORT)
+#define GRAPHICALRENDERING_API __declspec(dllexport)
+#else
+#define  GRAPHICALRENDERING_API __declspec(dllimport)
+#endif /* GRAPHICALRENDERING_API */
+#define _sprintf sprintf_s
+#endif
+
+#if defined(__GNUC__)
+//  GCC
+#define CUBE_API __attribute__((visibility("default")))
+#endif
+
+class GRAPHICALRENDERING_API Cube
+{
+public:
+	Cube();
+	static void RenderCube();
+	~Cube();
+};
+
+#endif //SOLARSYSTEMSETUP_CUBE_HPP

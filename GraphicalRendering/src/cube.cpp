f4db08bd76052e5af94fea3dd3e4d0cdca9d1ad7
ArturasDru <arturas.druteika@gmail.com>
2023-09-11 21:06:14 +0300

    Added proto imgui-vtk code but the code is very dirty

--- GraphicalRendering/src/cube.cpp
+++ GraphicalRendering/src/cube.cpp
@@ -17,78 +17,254 @@
 #include <array>
 #include "cube.hpp"
 
+// VTK
+//#include <vtkSmartPointer.h>
+//#include <vtkActor.h>
+
+// File-Specific Includes
+#include "imgui_vtk_demo.h" // Actor generator for this demo
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
 void Cube::RenderCube()
 {
-    vtkNew<vtkNamedColors> colors;
+    // Setup pipeline
+    auto actor = SetupDemoPipeline();
 
-    std::array<std::array<double, 3>, 8> pts = {{{{0, 0, 0}},
-                                                 {{1, 0, 0}},
-                                                 {{1, 1, 0}},
-                                                 {{0, 1, 0}},
-                                                 {{0, 0, 1}},
-                                                 {{1, 0, 1}},
-                                                 {{1, 1, 1}},
-                                                 {{0, 1, 1}}}};
-    // The ordering of the corner points on each face.
-    std::array<std::array<vtkIdType, 4>, 6> ordering = {{{{0, 3, 2, 1}},
-                                                         {{4, 5, 6, 7}},
-                                                         {{0, 1, 5, 4}},
-                                                         {{1, 2, 6, 5}},
-                                                         {{2, 3, 7, 6}},
-                                                         {{3, 0, 4, 7}}}};
-
-    // We'll create the building blocks of polydata including data attributes.
-    vtkNew<vtkPolyData> cube;
-    vtkNew<vtkPoints> points;
-    vtkNew<vtkCellArray> polys;
-    vtkNew<vtkFloatArray> scalars;
-
-    // Load the point, cell, and data attributes.
-    for (auto i = 0; i < pts.size(); ++i)
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
     {
-        points->InsertPoint(i, pts[i].data());
-        scalars->InsertTuple1(i, i);
-    }
-    for (auto&& i : ordering)
-    {
-        polys->InsertNextCell(vtkIdType(i.size()), i.data());
+        return;
     }
 
-    // We now assign the pieces to the vtkPolyData.
-    cube->SetPoints(points);
-    cube->SetPolys(polys);
-    cube->GetPointData()->SetScalars(scalars);
+#if __APPLE__
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
 
-    // Now we'll look at it.
-    vtkNew<vtkPolyDataMapper> cubeMapper;
-    cubeMapper->SetInputData(cube);
-    cubeMapper->SetScalarRange(cube->GetScalarRange());
-    vtkNew<vtkActor> cubeActor;
-    cubeActor->SetMapper(cubeMapper);
+// Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui VTKViewer Example", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        return;
+    }
+    //set minimum window size
+    glfwSetWindowSizeLimits(window, 1280, 720, GLFW_DONT_CARE, GLFW_DONT_CARE);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
 
-    // The usual rendering stuff.
-    vtkNew<vtkCamera> camera;
-    camera->SetPosition(1, 1, 1);
-    camera->SetFocalPoint(0, 0, 0);
 
-    vtkNew<vtkRenderer> renderer;
-    vtkNew<vtkRenderWindow> renWin;
-    renWin->AddRenderer(renderer);
-    renWin->SetWindowName("Cube");
+    // Decide GLSL version
+#ifdef __APPLE__
+    // GLSL 150
+    const char* glsl_version = "#version 150";
+#else
+    // GLSL 130
+    const char* glsl_version = "#version 130";
+#endif
 
-    vtkNew<vtkRenderWindowInteractor> iren;
-    iren->SetRenderWindow(renWin);
+    // Initialize OpenGL loader
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+    bool err = gl3wInit() != 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+    bool err = glewInit() != GLEW_OK;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+    bool err = gladLoadGL() == 0;
+#else
+    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+#endif
+    if (err)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return;
+    }
 
-    renderer->AddActor(cubeActor);
-    renderer->SetActiveCamera(camera);
-    renderer->ResetCamera();
-    renderer->SetBackground(colors->GetColor3d("Cornsilk").GetData());
+    // Initialize OpenGL loader
+    if (gl3wInit() != 0)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return;
+    }
 
-    renWin->SetSize(600, 600);
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows'
 
-    // interact with data
-    renWin->Render();
-    iren->Start();
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
+    // Initialize VtkViewer objects
+    VtkViewer vtkViewer1;
+    vtkViewer1.addActor(actor);
+
+    VtkViewer vtkViewer2;
+    vtkViewer2.getRenderer()->SetBackground(0, 0, 0); // Black background
+    vtkViewer2.addActor(actor);
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    bool vtk_2_open = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+        {
+            ImGui::ShowDemoWindow(&show_demo_window);
+        }
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin(
+                "Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Text(
+                "This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+            ImGui::Checkbox("VTK Viewer #2", &vtk_2_open);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))
+            {                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            }
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+                ImGui::GetIO().Framerate);
+        }
+        ImGui::End();
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window",
+                &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+            {
+                show_another_window = false;
+            }
+            ImGui::End();
+        }
+
+        // 4. Show a simple VtkViewer Instance (Always Open)
+        ImGui::SetNextWindowSize(ImVec2(360, 240), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Vtk Viewer 1", nullptr, VtkViewer::NoScrollFlags());
+        vtkViewer1.render(); // default render size = ImGui::GetContentRegionAvail()
+        ImGui::End();
+
+        // 5. Show a more complex VtkViewer Instance (Closable, Widgets in Window)
+        ImGui::SetNextWindowSize(ImVec2(720, 480), ImGuiCond_FirstUseEver);
+        if (vtk_2_open)
+        {
+            ImGui::Begin("Vtk Viewer 2", &vtk_2_open, VtkViewer::NoScrollFlags());
+
+            // Other widgets can be placed in the same window as the VTKViewer
+            // However, since the VTKViewer is rendered to size ImGui::GetContentRegionAvail(),
+            // it is best to put all widgets first (i.e., render the VTKViewer last).
+            // If you want the VTKViewer to be at the top of a window, you can manually calculate
+            // and define its size, accounting for the space taken up by other widgets
+
+            auto renderer = vtkViewer2.getRenderer();
+            if (ImGui::Button("VTK Background: Black"))
+            {
+                renderer->SetBackground(0, 0, 0);
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("VTK Background: Red"))
+            {
+                renderer->SetBackground(1, 0, 0);
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("VTK Background: Green"))
+            {
+                renderer->SetBackground(0, 1, 0);
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("VTK Background: Blue"))
+            {
+                renderer->SetBackground(0, 0, 1);
+            }
+            static float vtk2BkgAlpha = 0.2f;
+            ImGui::SliderFloat("Background Alpha", &vtk2BkgAlpha, 0.0f, 1.0f);
+            renderer->SetBackgroundAlpha(vtk2BkgAlpha);
+
+            vtkViewer2.render();
+
+            ImGui::End();
+        }
+
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Update and Render additional Platform Windows
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 
 Cube::Cube()
--- SolarSystemCore/src/ContextWindow.cpp
+++ SolarSystemCore/src/ContextWindow.cpp
@@ -15,6 +15,17 @@
 #include <string>
 #include <stdio.h>
 
+// Include glfw3.h after our OpenGL definitions
+#include "GLFW/glfw3.h"
+#include "VtkViewer.h"
+
+// VTK
+#include <vtkSmartPointer.h>
+#include <vtkActor.h>
+
+// File-Specific Includes
+#include "imgui_vtk_demo.h" // Actor generator for this demo
+
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
 // Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.

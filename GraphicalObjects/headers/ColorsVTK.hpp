c9fce20b1fd5b9f1421e149d8989e3b081ba4f77
ArturasDru <arturas.druteika@gmail.com>
2023-12-10 22:53:05 +0200

    Added color yellow

--- GraphicalObjects/headers/ColorsVTK.hpp
+++ GraphicalObjects/headers/ColorsVTK.hpp
@@ -11,6 +11,7 @@ struct ColorsVTK
     inline static const vtkColor4d GREEN = vtkColor4d(0.0, 1.0, 0.0, 1.0);
     inline static const vtkColor4d BLUE = vtkColor4d(0.0, 0.0, 1.0, 1.0);
     inline static const vtkColor4d RED = vtkColor4d(1.0, 0.0, 0.0, 1.0);
+    inline static const vtkColor4d YELLOW = vtkColor4d(1.0, 1.0, 0.0, 1.0);
 };
 
 

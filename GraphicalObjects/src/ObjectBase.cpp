81ef2f2dfa5ad55cf0dc3a90cbec75c3e04ac2bd
ArturasDru <arturas.druteika@gmail.com>
2023-12-20 21:11:01 +0200

    Left the hardcoded rotation, will need to fix it later!!!

--- GraphicalObjects/src/ObjectBase.cpp
+++ GraphicalObjects/src/ObjectBase.cpp
@@ -44,9 +44,8 @@ void ObjectBase::MoveActor(double xPos, double yPos, double zPos)
 
 void ObjectBase::RotateActor(double rotationDegrees)
 {
-    // m_actor->RotateX(rotationDegrees / 3);
-    // m_actor->RotateY(rotationDegrees / 3);
-    // m_actor->RotateZ(rotationDegrees / 3);
+    // Currently leaving it like this, but fix it later!!!
+    m_actor->RotateZ(rotationDegrees);
 }
 
 void ObjectBase::RotateX(double rotationDegrees)

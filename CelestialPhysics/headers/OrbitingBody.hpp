1682e8b54e82d2fd3db96ab46f9a2d441aab664f
ArtūrasDrūteika <arturas.druteika@gmail.com>
2024-11-15 21:35:56 +0100

    Add functionality to calculate orbital properties

--- CelestialPhysics/headers/OrbitingBody.hpp
+++ CelestialPhysics/headers/OrbitingBody.hpp
@@ -3,24 +3,46 @@
 
 
 #include "CosmicBody.hpp"
+#include "OrbitalPoint.hpp"
+#include <vector>
 
 
 class OrbitingBody : public CosmicBody
 {
 public:
-	OrbitingBody(double radius, double mass, double semiMajorAxis, double semiMinorAxis);
+	OrbitingBody(
+		double radius, 
+		double mass, 
+		const Point3D& focusObjectPt,
+		double focusObjectMass,
+		double semiMajorAxis, 
+		double semiMinorAxis, 
+		double inclination
+	);
 	~OrbitingBody();
 
 	double GetSemiMajorAxis();
 	double GetSemiMinorAxis();
 	double GetEccentricity();
+	std::vector<Point3D> GetOrbitalPoints();
+
 
 private:
 	void CalculateEccentricity();
+	void CalculateOrbitalPoints();
+	void CalculateGravitationalParameter(double focusObjectMass);
+	double CalculateOrbitalRadius(const Point3D& focusPt, const Point3D& bodyPoint);
+	double CalculateOrbitalSpeed(double orbitalRadius, double semiMajorAxis, double mu);
+	void CalculateOrbitalSpeedVec(const Point3D& focusPt);
 
 	double m_semiMajorAxis;
 	double m_semiMinorAxis;
 	double m_eccentricity;
+	double m_inclination;
+	double m_mu; // gravitational parameter
+	std::vector<Point3D> m_orbitalPoints;
+	std::vector<double> m_orbitalSpeedVec;
+	Point3D m_focusObjPoint;
 };
 
 
--- CelestialPhysics/src/OrbitingBody.cpp
+++ CelestialPhysics/src/OrbitingBody.cpp
@@ -1,14 +1,31 @@
 #include "OrbitingBody.hpp"
+#include "OrbitalMechanics.hpp"
+#include "PhysicalConstants.hpp"
 #include <stdexcept>
 #include <complex>
 
 
-OrbitingBody::OrbitingBody(double radius, double mass, double semiMajorAxis, double semiMinorAxis)
+constexpr double N_ORBIT_PTS = 10'000;
+
+
+OrbitingBody::OrbitingBody(
+	double radius, 
+	double mass, 
+	const Point3D& focusObjectPt,
+	double focusObjectMass, 
+	double semiMajorAxis, 
+	double semiMinorAxis, 
+	double inclination
+)
 	: CosmicBody(radius, mass)
 	, m_semiMajorAxis{ semiMajorAxis }
 	, m_semiMinorAxis{ semiMinorAxis }
+	, m_inclination{ inclination }
 {
 	CalculateEccentricity();
+	CalculateGravitationalParameter(focusObjectMass);
+	CalculateOrbitalPoints();
+	CalculateOrbitalSpeedVec(focusObjectPt);
 }
 
 OrbitingBody::~OrbitingBody()
@@ -30,6 +47,11 @@ double OrbitingBody::GetEccentricity()
 	return m_eccentricity;
 }
 
+std::vector<Point3D> OrbitingBody::GetOrbitalPoints()
+{
+	return m_orbitalPoints;
+}
+
 void OrbitingBody::CalculateEccentricity()
 {
 	if (m_semiMajorAxis <= 0 || m_semiMinorAxis <= 0 || m_semiMinorAxis > m_semiMajorAxis)
@@ -37,4 +59,43 @@ void OrbitingBody::CalculateEccentricity()
 		throw std::invalid_argument("Semi-major axis (a) must be greater than semi-minor axis (b), and both must be positive.");
 	}
 	m_eccentricity = std::sqrt(1 - (m_semiMinorAxis * m_semiMinorAxis) / (m_semiMajorAxis * m_semiMajorAxis));
+}
+
+void OrbitingBody::CalculateOrbitalPoints()
+{
+	m_orbitalPoints = OrbitalMechanics::CalculateOrbitPoints(
+		m_semiMajorAxis,
+		m_semiMinorAxis,
+		m_inclination,
+		N_ORBIT_PTS
+	);
+}
+
+void OrbitingBody::CalculateGravitationalParameter(double focusObjectMass)
+{
+	m_mu = GRAVITATIONAL_CONSTANT * focusObjectMass;
+}
+
+double OrbitingBody::CalculateOrbitalRadius(const Point3D& focusPt, const Point3D& bodyPoint)
+{
+	return std::sqrt(
+		std::pow(focusPt.x - bodyPoint.x, 2) +
+		std::pow(focusPt.y - bodyPoint.y, 2) +
+		std::pow(focusPt.z - bodyPoint.z, 2)
+	);
+}
+
+double OrbitingBody::CalculateOrbitalSpeed(double orbitalRadius, double semiMajorAxis, double mu)
+{
+	return std::sqrt(mu * (2.0 / orbitalRadius - 1.0 / semiMajorAxis));
+}
+
+void OrbitingBody::CalculateOrbitalSpeedVec(const Point3D& focusPt)
+{
+	for (const Point3D& orbitalPt : m_orbitalPoints)
+	{
+		double orbitalRadius = CalculateOrbitalRadius(focusPt, orbitalPt);
+		double orbitalSpeed = CalculateOrbitalSpeed(orbitalRadius, m_semiMajorAxis, m_mu);
+		m_orbitalSpeedVec.push_back(orbitalSpeed);
+	}
 }

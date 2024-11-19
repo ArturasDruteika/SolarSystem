0e8ca3c3aa3f1744d7b7d4c024aef462a170eace
ArturasDru <arturas.druteika@gmail.com>
2024-11-19 19:28:15 +0200

    Add funcs. to wrap solar system model

--- SolarSystemRendering/headers/SolarSystemVTKInteractor.hpp
+++ SolarSystemRendering/headers/SolarSystemVTKInteractor.hpp
@@ -12,6 +12,14 @@ public:
 	SolarSystemVTKInteractor();
 	~SolarSystemVTKInteractor();
 
+	void AddStar(int id, double starRadius);
+	void AddPlanet(int id, PlanetAttributes planetAttributes);
+	void OnDeleteStar(int id);
+	void OnDeletePlanet(int id);
+	int GetPlanetsCount() const;
+	std::map<int, Star> GetStarsMap() const;
+	std::map<int, Planet> GetPlanetsMap() const;
+
 	void Step();
 
 private:
--- SolarSystemRendering/src/SolarSystemVTKInteractor.cpp
+++ SolarSystemRendering/src/SolarSystemVTKInteractor.cpp
@@ -10,8 +10,45 @@ SolarSystemVTKInteractor::~SolarSystemVTKInteractor()
 {
 }
 
+void SolarSystemVTKInteractor::AddStar(int id, double starRadius)
+{
+	// TODO: add another param for star coordinates
+	m_solarSystemModel.AddStar(id, starRadius);
+}
+
+void SolarSystemVTKInteractor::AddPlanet(int id, PlanetAttributes planetAttributes)
+{
+	m_solarSystemModel.AddPlanet(id, planetAttributes);
+}
+
+void SolarSystemVTKInteractor::OnDeleteStar(int id)
+{
+	m_solarSystemModel.OnDeleteStar(id);
+}
+
+void SolarSystemVTKInteractor::OnDeletePlanet(int id)
+{
+	m_solarSystemModel.OnDeletePlanet(id);
+}
+
+int SolarSystemVTKInteractor::GetPlanetsCount() const
+{
+	return m_solarSystemModel.GetPlanetsCount();
+}
+
+std::map<int, Star> SolarSystemVTKInteractor::GetStarsMap() const
+{
+	return m_solarSystemModel.GetStarsMap();
+}
+
+std::map<int, Planet> SolarSystemVTKInteractor::GetPlanetsMap() const
+{
+	return m_solarSystemModel.GetPlanetsMap();
+}
+
 void SolarSystemVTKInteractor::Step()
 {
+	m_solarSystemModel.Step();
 	int i = 0;
 	if (i % 10'000 == 0)
 	{

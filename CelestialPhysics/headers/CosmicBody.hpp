7d07ac3660f504f1719005fe32ae4618a634cd86
ArturasDru <arturas.druteika@gmail.com>
2024-11-20 21:06:40 +0200

    Expose parent classes

--- CelestialPhysics/headers/CosmicBody.hpp
+++ CelestialPhysics/headers/CosmicBody.hpp
@@ -2,7 +2,22 @@
 #define CELESTIALPHYSICS_COSMICBODY_HPP
 
 
-class CosmicBody
+#if defined (_WIN32)
+#if defined(CELESTIALPHYSICS_EXPORT)
+#define CELESTIALPHYSICS_API __declspec(dllexport)
+#else
+#define  CELESTIALPHYSICS_API __declspec(dllimport)
+#endif /* CELESTIALPHYSICS_API */
+#define _sprintf sprintf_s
+#endif
+
+#if defined(__GNUC__)
+//  GCC
+#define OBJECTBASE_API __attribute__((visibility("default")))
+#endif
+
+
+class CELESTIALPHYSICS_API CosmicBody
 {
 public:
 	CosmicBody(double radius, double mass);

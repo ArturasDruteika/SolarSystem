19554586e357bd59e7b96d095292e405b6b3916d
ArturasDru <arturas.druteika@gmail.com>
2024-11-23 16:29:45 +0200

    Add earth days per year constant

--- CelestialPhysics/headers/PhysicalConstants.hpp
+++ CelestialPhysics/headers/PhysicalConstants.hpp
@@ -3,7 +3,7 @@
 
 
 constexpr double M_PI = 3.141'592'653'589'793'238'46;
-constexpr double EARTH_ROTATIONS_PER_YEAR = 365.242'189'7; // days
+constexpr double EARTH_ROTATIONS_PER_YEAR = 365.256'363'004; // days
 constexpr double GRAVITATIONAL_CONSTANT = 6.67430e-11; // m^3 kg^-1 s^-2
 constexpr double N_ORBIT_PTS = 10'000;
 constexpr int ECCENTRIC_ANOMALY_ITERATIONS = 10'000;
